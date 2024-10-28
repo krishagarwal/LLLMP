28626958fc328271ae1a35fbc8f241bea575ea66
Krish Agarwal <krishagarwal@live.com>
2024-10-28 12:10:50 -0500

    timing planner

--- dataset/populate_plans.py
+++ dataset/populate_plans.py
@@ -1,6 +1,7 @@
 from dataset import Dataset
 import os
 from pathlib import Path
+import time
 
 project_dir = Path(__file__).parent.parent.as_posix()
 
@@ -13,11 +14,16 @@ def main():
         print("Planning for time step", time_step["time"])
         problem_file = time_step['problem_path']
         plan_file = os.path.join(Path(problem_file).parent, "true_plan.pddl")
+        
+        start_time = time.time()
         os.system(f"sudo docker run --rm -v {project_dir}:/root/experiments lapkt/lapkt-public ./siw-then-bfsf " + \
             f"--domain /root/experiments/{dataset.domain_path} " + \
             f"--problem /root/experiments/{time_step['problem_path']} " + \
             f"--output /root/experiments/{plan_file} " + \
             f"> /dev/null")
+        duration = time.time() - start_time
+        with open(os.path.join(Path(problem_file).parent, "true_plan.time"), "w") as f:
+            f.write(str(duration))
         
     print("Finished planning")
 
